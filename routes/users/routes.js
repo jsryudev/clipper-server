ad202d8107d702bfd9cdd8f2d93dfde64b3a7107
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-25 23:49:30 +0900

    GET /user/:id, POST /user 구현

--- routes/users/routes.js
+++ routes/users/routes.js
@@ -4,12 +4,37 @@ const express = require('express');
 const User = require('./model');
 const router = express.Router();
 
+// Get all users
 router.get('/', async (req, res) => {
   try {
     const result = await User.find({});
     res.json(result);
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(400).send(error.message);
+  }
+});
+
+// Get a user
+router.get('/:id', async (req, res) => {
+  try {
+    const result = await User.findById(req.params.id);
+    if (!result) {
+      res.sendStatus(404);
+    }
+    res.json(result);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+// Create a user
+router.post('/', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 });
 
