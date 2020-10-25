412aa6ee80e46359edfa224904ebfc577968001f
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-26 01:55:38 +0900

    Clip model schema 정의

---
+++ routes/clips/model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const clipSchema = new mongoose.Schema({
+  coordinate: {
+    longitude: {
+      type: Number,
+      require: true,
+    },
+    latitude: {
+      type: Number,
+      require: true,
+    },
+  },
+  location: {
+    type: String,
+    default: null,
+  },
+  title: {
+    type: String,
+    default: null,
+  },
+  note: {
+    type: String,
+    default: null,
+  },
+  author: {
+    type: mongoose.ObjectId,
+    ref: 'User',
+    require: true,
+  },
+  likedUsers: [
+    {
+      type: mongoose.ObjectId,
+      ref: 'User',
+    },
+  ],
+  dislikedUsers: [
+    {
+      type: mongoose.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+const Clip = mongoose.model('Clip', clipSchema);
+
+module.exports = Clip;
