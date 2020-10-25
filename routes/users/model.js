0a322400168abde4b07e3b9df6ecc29776a03244
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-25 23:22:16 +0900

    User Model Schema 정의

---
+++ routes/users/model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    default: null,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
