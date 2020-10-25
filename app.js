38be8a6301eb9ef576ba8e094af2ee7e486a91f4
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-25 21:54:34 +0900

    Mongoose 설정

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+'use strict';
+
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 // routers
 const users = require('./routes/users');
 
-var app = express();
+mongoose.connect(process.env.CLIPPER_MONGO, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const app = express();
+const db = mongoose.connection;
+
+db.on('error', err => {
+  console.error(err);
+});
+db.once('connected', () => {
+  console.log('Mongo connected!');
+});
 
 app.use(logger('dev'));
 app.use(express.json());
