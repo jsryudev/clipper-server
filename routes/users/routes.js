03ceee1b891dd8dab8022ae9864121db21e46984
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-25 23:22:36 +0900

    GET /users 구현

--- routes/users/routes.js
+++ routes/users/routes.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const express = require('express');
+const User = require('./model');
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.send('hello, world!');
+router.get('/', async (req, res) => {
+  try {
+    const result = await User.find({});
+    res.json(result);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 });
 
 module.exports = router;
