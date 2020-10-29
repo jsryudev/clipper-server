3a0e8a7e6bd9a3a8e88b8ff60f3ba7376557e0df
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-30 01:22:09 +0900

    Google id 로그인 적용을 위한 /user/google-id 라우터 추가

--- routes/user/routes.js
+++ routes/user/routes.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 const User = require('../users/model');
 
 const router = express.Router();
@@ -13,6 +14,24 @@ router.get('/', async (req, res) => {
       res.sendStatus(404);
     }
     const result = _.omit(user.toObject(), ['identifier', '__v']);
+    res.json(result);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.post('/google-id', async (req, res) => {
+  // Google ID Token을 기준으로 userID 검증
+
+  try {
+    const user = await User.findOne({ identifier: '' });
+    if (!user) {
+      res.sendStatus(404);
+    }
+
+    const result = _.omit(user.toObject(), ['identifier', '__v']);
+    result.accessToken = jwt.sign(result, process.env.CLIPPER_JWT_SECRET);
+
     res.json(result);
   } catch (error) {
     res.status(400).send(error.message);
