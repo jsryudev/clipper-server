bdcb3e742aefd04980b3f20a1c3a57d1607e7004
JunSang Ryu <jsryu.dev@gmail.com>
2020-11-11 21:12:59 +0900

    markers router 추가

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const router = express.Router();
 
 const user = require('./user/routes');
 const users = require('./users/routes');
+const markers = require('./markers/routes');
 const clips = require('./clips/routes');
 
 router.use('/user', user);
 router.use('/users', users);
+router.use('/markers', markers);
 router.use('/clips', clips);
 
 module.exports = router;
